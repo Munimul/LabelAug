2bab310991adb368a1feb798c1ae3bb453fe72d2
munimul hasan <munimul999@gmail.com>
2024-01-23 22:46:04 -0600

    some error messages added, gocheck and auglistcheck functions added

--- labelaug.py
+++ labelaug.py
@@ -1,7 +1,7 @@
 import sys
 import glob
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QTextEdit, QVBoxLayout, QCheckBox, QFileDialog, QLabel
+from PyQt6.QtWidgets import  QApplication, QWidget, QPushButton, QVBoxLayout, QCheckBox, QFileDialog, QLabel
 
 class MyApp(QWidget):
     def __init__(self):
@@ -12,33 +12,42 @@ class MyApp(QWidget):
 #label open button with 
         labelOpen=QPushButton("Label Open Directory", clicked=self.openDirectory)
 # All augment checkboxs (add more augmentations)       
-        self.rotate=QCheckBox(text='rotate')
-        self.flip=QCheckBox(text='flip')
+        self.rotateC90=QCheckBox(text='rotateC90')
+        self.rotateC180=QCheckBox(text='rotateC180')
+        self.rotateC270=QCheckBox(text='rotateC270')
+        self.flipOnY=QCheckBox(text='flipOnY')
 # Two variables to save the directories
         self.openDir=None
         self.saveDir=None
+# List of all augmentation
+        self.listAug=['rotateC90','rotateC180','rotateC270','flipOnY']
+# List of to do augmentation
+        self.toDoAugList=[]
 # All textfiles locations
         self.textfiles=None
 
 # labelbox to show the open directory path and file info
-        self.openDir=QLabel()
+        self.openDirLabel=QLabel()
         self.openDirInfo=QLabel()
 # Label save directory button
         labelSave=QPushButton("Label Save Directory", clicked=self.saveDirectory)
 # Textbox to show the save directory path
-        self.saveDir=QLabel()
+        self.saveDirLabel=QLabel()
         self.saveDirInfo=QLabel()
 # Execute button to augment labels for checked items
         goButton=QPushButton("Go!", clicked=self.goFunctions)
 
         
         layout.addWidget(labelOpen)
-        layout.addWidget(self.openDir)
+        layout.addWidget(self.openDirLabel)
         layout.addWidget(self.openDirInfo)
-        layout.addWidget(self.rotate),layout.addWidget(self.flip)
+        layout.addWidget(self.rotateC90)
+        layout.addWidget(self.rotateC180)
+        layout.addWidget(self.rotateC270)
+        layout.addWidget(self.flipOnY)
         
         layout.addWidget(labelSave)
-        layout.addWidget(self.saveDir)
+        layout.addWidget(self.saveDirLabel)
         layout.addWidget(self.saveDirInfo)
         layout.addWidget(goButton)
 
@@ -50,9 +59,10 @@ class MyApp(QWidget):
 
 
     def openDirectory(self):
+        self.saveDirInfo.setText('')
         dialog = QFileDialog()
         foo_dir = dialog.getExistingDirectory(self,'Select a Folder')
-        self.openDir.setText(foo_dir)
+        self.openDirLabel.setText(foo_dir)
         self.openDir=foo_dir
         self.textfiles=glob.glob(self.openDir+'/*.txt')
         if((len(self.textfiles))>0):
@@ -62,17 +72,56 @@ class MyApp(QWidget):
             self.openDirInfo.setText('No .txt files found in the directory!')
         return
     def saveDirectory(self):
+        self.saveDirInfo.setText('')
         dialog = QFileDialog()
         foo_dir = dialog.getExistingDirectory(self,'Select a Folder')
-        self.saveDir.setText(foo_dir)
+        self.saveDirLabel.setText(foo_dir)
         self.saveDir=foo_dir
         return
     def goFunctions(self):
+#Check if labeldir is valid or contains .txt files
+#Check if augmentation is selected or not
+#Check if savedir is valid
+        self.checkBoxStatus()
+        self.saveDirInfo.setText('')
+        if self.goCheck():
+            print('ready !')
+# Add functions to execute the augmentations '''''''''''ToDO'''''''''''
+        else:
+            print('Not ready')
 
-        if self.rotate.isChecked():
-            print('rotate is on')
-        if self.flip.isChecked():
-            print('flip is on')
+
+    def checkBoxStatus(self):
+        self.toDoAugList=[]
+        if self.rotateC90.isChecked():
+            self.toDoAugList.append('rotateC90')
+        if self.rotateC180.isChecked():
+            self.toDoAugList.append('rotateC180')
+        if self.rotateC270.isChecked():
+            self.toDoAugList.append('rotateC270')
+        if self.flipOnY.isChecked():
+            self.toDoAugList.append('flipOnY')
+    
+    def goCheck(self):
+ 
+        if self.openDir==None:
+            self.saveDirInfo.setText('No label directory is selected!')
+            return False
+        if (len(self.textfiles)==0):
+            self.saveDirInfo.setText('No .txt file in the directory to augment!')
+            return False
+        if (len(self.toDoAugList)==0):
+            self.saveDirInfo.setText('No Augment to be done! Select augment from the checklist!')
+            return False
+        if self.saveDir==None:
+            self.saveDirInfo.setText('No save directory is selected!')
+            return False
+        return True
+            
+            
+         
+
+
     
 
 
